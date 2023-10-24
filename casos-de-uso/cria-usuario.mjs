f1c01b59b7e0cc74e91c3ccadb4942097e56cd4a
Beatriz Campos <clbeatriz244@gmail.com>
2023-10-24 10:25:34 -0300

    Modificações para criar usuário utilizando a nova classe criada

--- casos-de-uso/cria-usuario.mjs
+++ casos-de-uso/cria-usuario.mjs
@@ -1,9 +1,11 @@
-import { v4 as uuid } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
+import { EntidadeConta } from "../entidades/conta.entity.mjs";
 
 let listaContas = []
 
 export function criaUsuario(nome, email, senha) {
-    const data = new Date();
+    const usuario = new EntidadeConta(nome, email,senha)
+    /*const data = new Date();
     const id = uuid();
 
     const usuario = {
@@ -12,7 +14,7 @@ export function criaUsuario(nome, email, senha) {
         senha: senha,
         dataCriacao: data,
         id: id
-    };
+    };*/
 
     listaContas.push(usuario);
 
