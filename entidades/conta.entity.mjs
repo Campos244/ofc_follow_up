dd29325cc891291c83f25fd727ada5e30b0554e0
Beatriz Campos <clbeatriz244@gmail.com>
2023-10-24 10:23:37 -0300

    Adiciona pasta entidades para a criação classe

---
+++ entidades/conta.entity.mjs
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class EntidadeConta{
+nome
+email
+senha
+data
+id
+
+
+constructor (nome, email,senha) {
+    this.nome = nome
+    this.email = email
+    this.senha = senha
+    this.data = new Date();
+    this.id = uuidv4();
+}
+}
+
+const usuario = new EntidadeConta()
+console.log(usuario)
