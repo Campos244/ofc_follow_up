3e3683f088e9f0bfe1f7d6b35f7e697cbb0c7897
Beatriz Campos <clbeatriz244@gmail.com>
2023-11-09 11:46:37 -0300

    cria usuarios a partir das classes criadas

--- dados/conta-repository-tests.mjs
+++ dados/conta-repository-tests.mjs
@@ -1,20 +1,25 @@
-import { criaUsuario } from "../casos-de-uso/cria-usuario.mjs";
+import { CriaUsuario } from "../casos-de-uso/cria-usuario.mjs";
 import { salva } from "./contas.repository.mjs";
 import { lista } from "./contas.repository.mjs";
 
 
-criaUsuario("Maria Santana", "santanaMarina@email.com", "senha");
+/*criaUsuario("Maria Santana", "santanaMarina@email.com", "senha");
 criaUsuario("Beatriz Campos", "beatriz@gmail.com", "senha123");
 criaUsuario("Pandinha fofa", "panda", "far");
 criaUsuario("Tatiane Campos", "tati.24@outlook.com", "tatitatonilda")
 criaUsuario("Alura Alurinha Alurona", "alura@alura.com", "cael")
 criaUsuario("Bruno", "bruno@gmail.com", "BRUNO1234")
 salva();
-lista();
+lista();*/
 
 
 
+const contaRepo = new ContaRepository();
+const criaUsuarioCasoDeUso = new CriaUsuario(contaRepo);
 
+criaUsuarioCasoDeUso.executa('João', 'joao@email.com', 'senha123')
+    .then((texto) => console.log(texto))
+    .catch((error) => console.error(error));
 
 
 
