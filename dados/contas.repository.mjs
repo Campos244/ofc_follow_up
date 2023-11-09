d185da75a74406af1568141ab284ac9bd76949d4
Beatriz Campos <clbeatriz244@gmail.com>
2023-11-09 11:48:10 -0300

    Exporta classe ContaRepository que salva e lista os usuarios no arquivo json

--- dados/contas.repository.mjs
+++ dados/contas.repository.mjs
@@ -2,7 +2,7 @@ import { criaUsuario } from "../casos-de-uso/cria-usuario.mjs";
 import { error } from "console";
 import fs from "fs";
 
-export function salva() {
+/*export function salva() {
 
     let contas = criaUsuario();
 
@@ -14,11 +14,36 @@ export function salva() {
     } catch (error) {
         console.error("Erro ao salvar o arquivo:", error);
     }
-}
+}*/
 
-export function lista() {
+/*export function lista() {
     const encoding = 'utf-8'
     fs.promises.readFile("dados/contas.json", encoding)
         .then((texto) => console.log(texto))
         .catch(error)
+}*/
+
+
+export class ContaRepository {
+    constructor() {
+        this.listaContas = [];
+    }
+
+    salva(usuario) {
+        this.listaContas.push(usuario);
+
+        try {
+            const contaJson = JSON.stringify(this.listaContas);
+
+            fs.writeFileSync("dados/contas.json", contaJson);
+            console.log("Arquivo salvo com sucesso!");
+        } catch (error) {
+            console.error("Erro ao salvar o arquivo:", error);
+        }
+    }
+
+    lista() {
+        const encoding = 'utf-8';
+        return fs.promises.readFile("dados/contas.json", encoding);
+    }
 }
