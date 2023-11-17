d478db1529e9c2ea574925308af9c74a1b019a65
Beatriz Campos <clbeatriz244@gmail.com>
2023-11-17 15:58:28 -0300

    Adiciona classe entidadeConta para a criação do usuário

--- entidades/conta.entity.mjs
+++ entidades/conta.entity.mjs
@@ -1,23 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export class EntidadeConta{
-nome
-email
-senha
-data
-id
+export class EntidadeConta {
+    nome;
+    email;
+    senha;
+    data;
+    id;
 
-
-constructor (nome, email,senha) {
-    this.nome = nome
-    this.email = email
-    this.senha = senha
-    this.data = new Date();
-    this.id = uuidv4();
+    constructor(nome, email, senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.data = new Date();
+        this.id = uuidv4();
+    }
 }
 
-
-}
-
-const usuario = new EntidadeConta()
-console.log(usuario)
