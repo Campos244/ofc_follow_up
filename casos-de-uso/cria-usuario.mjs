59bf7a8cdb7988b61088f89d5d577b26d58bbd68
Beatriz Campos <clbeatriz244@gmail.com>
2023-11-17 15:54:22 -0300

    adiciona classe para criação de usuario

--- casos-de-uso/cria-usuario.mjs
+++ casos-de-uso/cria-usuario.mjs
@@ -1,16 +1,14 @@
-
 import { EntidadeConta } from "../entidades/conta.entity.mjs";
-import { ContaRepository } from "../dados/contas.repository.mjs";
+import { ContaRepository } from "../dados/contas-repository.mjs";
 
-export class CriaUsuario{
-    constructor(ContaRepository){
+export class CriaUsuario {
+    constructor(ContaRepository) {
         this.ContaRepository = ContaRepository;
     }
 
-    executa(nome, email, senha){
+    executa(nome, email, senha) {
         const novoUsuario = new EntidadeConta(nome, email, senha);
         this.ContaRepository.salva(novoUsuario);
         return this.ContaRepository.lista();
     }
-
 }
