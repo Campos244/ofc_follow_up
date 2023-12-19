a6c907b1a1ce81aba3632ffb6cc574b0d52950df
Beatriz Campos <clbeatriz244@gmail.com>
2023-12-19 10:54:28 -0300

    Valida contas dos usuários

--- validadores/cria-conta-validador.mjs
+++ validadores/cria-conta-validador.mjs
@@ -1,9 +1,6 @@
 // @ts-ignore
-import lista from "../dados/contas.json" assert { type: "json" };
-import { ContaRepository } from "../dados/contas-repository.mjs";
-import { criaUsuarioCasoDeUso } from "../dados/conta-repository-tests.mjs";
 import fs from "fs";
-let erros = [];
+
 
 /*function criaContaValidador(lista) {
     const validacoes = lista.map(objeto => {
@@ -26,83 +23,203 @@ criaContaValidador(ContaRepository)*/
 
 // Carrega o conteúdo do arquivo lista.json
 
-
-
-
-
-
-
-class CriaContaValidador {
+/*class CriaContaValidador {
     constructor(filePath) {
         this.filePath = filePath;
+        this.listaDeObjetos = []; // Inicializa listaDeObjetos como um array vazio
     }
-
 
     validarSenha(senha) {
         if (typeof senha === 'string' && senha.length >= 8) {
+            this.listaDeObjetos.push(senha);
             return senha;
         } else {
-            return ({
-                temErro: true, campo: "Senha",
+            return {
+                temErro: true,
+                campo: "Senha",
                 mensagem: "Senha menor que oito dígitos."
-            });
+            };
         }
     }
 
     validarEmail(email) {
         const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (regexEmail.test(email) == true) {
+        if (regexEmail.test(email)) {
+            this.listaDeObjetos.push(email);
             return email;
         } else {
-            return ({
-                temErro: true, campo: "Email",
+            return {
+                temErro: true,
+                campo: "Email",
                 mensagem: "Email inválido"
-            });
+            };
         }
-
     }
 
     validarNome(nome) {
         if (typeof nome === 'string' && nome.trim() !== "") {
+            this.listaDeObjetos.push(nome);
             return nome;
         } else {
-            return ({
-                temErro: true, campo: "Nome",
+            return {
+                temErro: true,
+                campo: "Nome",
                 mensagem: "Nome não pode ser vazio"
-            });
+            };
         }
     }
 
-    executa(){
+    async executa() {
         try {
             const data = fs.readFileSync(this.filePath, 'utf8');
-            const listaDeObjetos = JSON.parse(data);
-        
-            const validacoes = listaDeObjetos.map(objeto => {
+            this.listaDeObjetos = JSON.parse(data);
+            const validacoes = this.listaDeObjetos.map(objeto => {
                 const dados = {
-                    temErro: false,
-                    erros: [],
                     nome: this.validarNome(objeto.nome),
                     email: this.validarEmail(objeto.email),
                     senha: this.validarSenha(objeto.senha)
                 };
                 return dados;
             });
-        
-            console.log(validacoes);
-            return validacoes;
+    
+            console.log("Validações:", validacoes);
+            const contasValidas = this.descartaContasInvalidas(validacoes);
+    
+            return validacoes; // Retorna 'validacoes' opcionalmente se necessário
         } catch (error) {
             console.error('Erro ao ler ou validar os dados:', error);
             return [];
         }
     }
-}
     
 
+     descartaContasInvalidas(validacoes) {
+        // Implemente a lógica para filtrar as contas inválidas aqui
+        if (!Array.isArray(validacoes)) {
+            console.error('Os dados de validação são inválidos ou estão ausentes.');
+            return [];
+        }
 
-const filePath = 'dados/contas.json';
-const validador = new CriaContaValidador(filePath);
+        const contasValidas = validacoes.filter(dados => dados.temErro == false);
+        console.log("Contas válidas:", contasValidas);
+
+        const filePathValidate = 'dados/contasValidadas.json';
+        try {
+             fs.promises.writeFile(filePathValidate, JSON.stringify(contasValidas, null, 2));
+            console.log("Contas válidas salvas com sucesso no arquivo!");
+        } catch (error) {
+            console.error('Erro ao salvar as contas válidas:', error);
+        }
+    }
+}
+
+const filePathInvalidate = 'dados/contas.json';
+const validador = new CriaContaValidador(filePathInvalidate);
 validador.executa();
+
+
+//const contasValidas = validador.descartaContasInvalidas(validacoes);
+// validador.salvarContasNoArquivo(contasValidas);
+
+*/
+
+
+
+class CriaContaValidador {
+    constructor(filePath) {
+        this.filePath = filePath;
+        this.listaDeObjetos = [];
+    }
+
+    validarSenha(senha) {
+        if (typeof senha === 'string' && senha.length >= 8) {
+            this.listaDeObjetos.push(senha);
+            return senha;
+        } else {
+            const dados = {
+                temErro: true,
+                campo: "Senha",
+                mensagem: "Senha menor que oito dígitos."
+            };
+            return dados
+        }
+    }
+
+    validarEmail(email) {
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (regexEmail.test(email)) {
+            this.listaDeObjetos.push(email);
+            return email;
+        } else {
+            const dados = {
+                temErro: true,
+                campo: "Email",
+                mensagem: "Email inválido"
+            };
+            return dados
+        }
+    }
+
+    validarNome(nome) {
+        if (typeof nome === 'string' && nome.trim() !== "") {
+            this.listaDeObjetos.push(nome);
+            return nome;
+        } else {
+            const dados = {
+                temErro: true,
+                campo: "Nome",
+                mensagem: "Nome não pode ser vazio"
+            };
+            return dados
+        }
+    }
+
+    async executa() {
+        try {
+            const data = fs.readFileSync(this.filePath, 'utf8');
+            this.listaDeObjetos = JSON.parse(data);
+            const validacoes = this.listaDeObjetos.map(objeto => {
+                const dados = {
+                    nome: this.validarNome(objeto.nome) ? objeto.nome : { temErro: true, campo: "Nome", mensagem: "Nome não pode ser vazio" },
+                    email: this.validarEmail(objeto.email) ? objeto.email : { temErro: true, campo: "Email", mensagem: "Email inválido" },
+                    senha: this.validarSenha(objeto.senha)
+                };
+                return dados;
+            });
+    
+            console.log("Validações:", validacoes);
+            await this.descartaContasInvalidas(validacoes);
+    
+            return validacoes; // Retorna 'validacoes' opcionalmente se necessário
+        } catch (error) {
+            console.error('Erro ao ler ou validar os dados:', error);
+            return [];
+        }
+    }
+
+    async descartaContasInvalidas(validacoes) {
+        if (!Array.isArray(validacoes)) {
+            console.error('Os dados de validação são inválidos ou estão ausentes.');
+            return;
+        }
+
+        const contasValidas = validacoes.filter(dados => !dados.nome.temErro && !dados.email.temErro && !dados.senha.temErro);
+        console.log("Contas válidas:", contasValidas);
+
+        const filePathValidate = 'dados/contasValidadas.json';
+        try {
+            await fs.promises.writeFile(filePathValidate, JSON.stringify(contasValidas, null, 2));
+            console.log("Contas válidas salvas com sucesso no arquivo!");
+        } catch (error) {
+            console.error('Erro ao salvar as contas válidas:', error);
+        }
+    }
+}
+
+const filePathInvalidate = 'dados/contas.json';
+const validador = new CriaContaValidador(filePathInvalidate);
+validador.executa();
+
 
 
 
