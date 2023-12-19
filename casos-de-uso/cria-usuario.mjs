cb1d2265361530494d88b4e9b73bbc2808fc4bb7
Beatriz Campos <clbeatriz244@gmail.com>
2023-12-19 10:47:16 -0300

    Atualiza código para funcionamento das validações

--- casos-de-uso/cria-usuario.mjs
+++ casos-de-uso/cria-usuario.mjs
@@ -1,5 +1,9 @@
 import { EntidadeConta } from "../entidades/conta.entity.mjs";
 import { ContaRepository } from "../dados/contas-repository.mjs";
+/*import { EntidadePublicacao } from "../entidades/conta.entity.mjs";
+
+import { Postagens } from "../dados/contas-repository.mjs";*/
+
 
 export class CriaUsuario {
     constructor(ContaRepository) {
@@ -11,4 +15,19 @@ export class CriaUsuario {
         this.ContaRepository.salva(novoUsuario);
         return this.ContaRepository.lista();
     }
+
 }
+
+
+export class CriaPostagem {
+    constructor(postagens) {
+        this.postagens = postagens;
+    }
+
+    executaPostagem(autor, mensagem) {
+        const novaPostagem = { autor, mensagem };
+        this.postagens.posta(novaPostagem);
+        return novaPostagem;
+    }
+}
+
